0bf54fc6d2a48b10ea10ed1e95e50bc8d19bb276
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2018-06-18 11:35:16 -0400

    Initial working version!

--- swebclient/Main.Designer.vb
+++ swebclient/Main.Designer.vb
@@ -24,8 +24,11 @@ Partial Class Main
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
         Me.Menu = New System.Windows.Forms.MenuStrip()
-        Me.HomeButton = New System.Windows.Forms.Button()
         Me.AppFrame = New System.Windows.Forms.WebBrowser()
+        Me.TitleLeader = New System.Windows.Forms.Label()
+        Me.Title = New System.Windows.Forms.Label()
+        Me.RefreshButton = New System.Windows.Forms.Button()
+        Me.HomeButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Menu
@@ -37,6 +40,52 @@ Partial Class Main
         Me.Menu.Size = New System.Drawing.Size(800, 24)
         Me.Menu.TabIndex = 0
         Me.Menu.Text = "Menu"
+        '
+        'AppFrame
+        '
+        Me.AppFrame.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.AppFrame.Location = New System.Drawing.Point(0, 24)
+        Me.AppFrame.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.AppFrame.Name = "AppFrame"
+        Me.AppFrame.Size = New System.Drawing.Size(800, 426)
+        Me.AppFrame.TabIndex = 2
+        Me.AppFrame.Url = New System.Uri("https://sykeben.github.io/swebserver/", System.UriKind.Absolute)
+        '
+        'TitleLeader
+        '
+        Me.TitleLeader.AutoSize = True
+        Me.TitleLeader.BackColor = System.Drawing.Color.Gainsboro
+        Me.TitleLeader.Location = New System.Drawing.Point(59, 5)
+        Me.TitleLeader.Name = "TitleLeader"
+        Me.TitleLeader.Size = New System.Drawing.Size(72, 13)
+        Me.TitleLeader.TabIndex = 3
+        Me.TitleLeader.Text = "Current Page:"
+        '
+        'Title
+        '
+        Me.Title.AutoSize = True
+        Me.Title.BackColor = System.Drawing.Color.Gainsboro
+        Me.Title.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Title.Location = New System.Drawing.Point(132, 5)
+        Me.Title.Name = "Title"
+        Me.Title.Size = New System.Drawing.Size(14, 13)
+        Me.Title.TabIndex = 4
+        Me.Title.Text = "?"
+        '
+        'RefreshButton
+        '
+        Me.RefreshButton.BackColor = System.Drawing.Color.Gainsboro
+        Me.RefreshButton.BackgroundImage = Global.swebclient.My.Resources.Resources.Refresh_View_16x16
+        Me.RefreshButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.RefreshButton.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro
+        Me.RefreshButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver
+        Me.RefreshButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray
+        Me.RefreshButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.RefreshButton.Location = New System.Drawing.Point(28, 0)
+        Me.RefreshButton.Name = "RefreshButton"
+        Me.RefreshButton.Size = New System.Drawing.Size(23, 23)
+        Me.RefreshButton.TabIndex = 5
+        Me.RefreshButton.UseVisualStyleBackColor = False
         '
         'HomeButton
         '
@@ -53,22 +102,15 @@ Partial Class Main
         Me.HomeButton.TabIndex = 1
         Me.HomeButton.UseVisualStyleBackColor = False
         '
-        'AppFrame
-        '
-        Me.AppFrame.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.AppFrame.Location = New System.Drawing.Point(0, 24)
-        Me.AppFrame.MinimumSize = New System.Drawing.Size(20, 20)
-        Me.AppFrame.Name = "AppFrame"
-        Me.AppFrame.Size = New System.Drawing.Size(800, 426)
-        Me.AppFrame.TabIndex = 2
-        Me.AppFrame.Url = New System.Uri("https://sykeben.github.io/swebserver/", System.UriKind.Absolute)
-        '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.WhiteSmoke
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RefreshButton)
+        Me.Controls.Add(Me.Title)
+        Me.Controls.Add(Me.TitleLeader)
         Me.Controls.Add(Me.AppFrame)
         Me.Controls.Add(Me.HomeButton)
         Me.Controls.Add(Me.Menu)
@@ -84,4 +126,7 @@ Partial Class Main
     Friend WithEvents Menu As MenuStrip
     Friend WithEvents HomeButton As Button
     Friend WithEvents AppFrame As WebBrowser
+    Friend WithEvents TitleLeader As Label
+    Friend WithEvents Title As Label
+    Friend WithEvents RefreshButton As Button
 End Class
--- swebclient/Main.vb
+++ swebclient/Main.vb
@@ -1,5 +1,55 @@
 ﻿Public Class Main
+    Dim Quitting As Boolean
+
     Private Sub HomeButton_Click(sender As Object, e As EventArgs) Handles HomeButton.Click
-        AppFrame.Navigate("https://sykeben.github.io/swebserver/")
+        If Not AppFrame.Url = New Uri("https://sykeben.github.io/swebserver/") Then
+            Dim Answer As DialogResult = MessageBox.Show("Are you sure you want to go home?",
+            "Confirm", MessageBoxButtons.YesNo)
+            Select Case Answer
+                Case Windows.Forms.DialogResult.Yes
+                    AppFrame.Navigate("https://sykeben.github.io/swebserver/")
+            End Select
+        End If
+    End Sub
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Quitting = False
+    End Sub
+
+    Private Sub Main_FormClosing(ByVal sender As Object,
+    ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If Quitting = False Then
+            Dim Answer As DialogResult = MessageBox.Show("Are you sure you want to quit?",
+            "Confirm", MessageBoxButtons.YesNo)
+            Select Case Answer
+                Case Windows.Forms.DialogResult.Yes
+                    e.Cancel = False
+                    Quitting = True
+                    Application.Exit()
+                Case Windows.Forms.DialogResult.No
+                    Quitting = False
+                    e.Cancel = True
+                Case Else
+                    Quitting = False
+                    e.Cancel = True
+            End Select
+        End If
+    End Sub
+
+    Private Sub AppFrame_Navigated(sender As Object, e As WebBrowserNavigatedEventArgs) Handles AppFrame.Navigated
+        Title.Text = AppFrame.DocumentTitle
+    End Sub
+
+    Private Sub AppFrame_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles AppFrame.Navigating
+        Title.Text = "Loading..."
+    End Sub
+
+    Private Sub RefreshButton_Click(sender As Object, e As EventArgs) Handles RefreshButton.Click
+        Dim Answer As DialogResult = MessageBox.Show("Are you sure you want to refresh?",
+            "Confirm", MessageBoxButtons.YesNo)
+        Select Case Answer
+            Case Windows.Forms.DialogResult.Yes
+                AppFrame.Refresh()
+        End Select
     End Sub
 End Class
